b7545d2255d6cbee71dbacfb6b72e8e5a613fa7f
stolenhead <andrea.palomino.ruby@gmail.com>
2016-09-27 00:33:26 -0500

    entendiendo js txtarea

--- index.js
+++ index.js
@@ -1,50 +1,92 @@
+window.addEventListener("load", function(){
+  var contenedorPadre= document.getElementById("contenedor");
+  var padre = document.getElementById("papa");
+  var lista = document.getElementById("papa2");
+    var formulario = document.getElementById("formu");
+    var button = document.getElementById("boton");
+  var div = document.getElementById("clicky");
+  var i = 0;
+    div.addEventListener("click", function() {
+      agregar(formulario, div);
 
-var board = document.getElementById("clicky");
-var linky =document.getElementById("linky");
-var boton =document.getElementById("boton");
-var forms =document.getElementById("formu");
-var padre=document.getElementById("linky");
-var padre2=document.getElementById("papa2");
-var input =document.getElementById("input");
+    });
 
-window.addEventListener("load", function() {
-	board.addEventListener("click", boards);
-    function boards() {
-      board.classList.add("none");
-      linky.classList.add("none");
-      forms.classList.remove("none");
-    };
-    boton.addEventListener("click",botons);
-    function botons(e){
+    button.addEventListener("click", function(e){
       e.preventDefault();
-        crear();
-        añadir();
-    }; 
-    function crear(){  
-      forms.classList.add("none");
-      board.classList.remove("none");
-      linky.classList.remove("none");
-      linky.innerHTML=input.value;
-      var newDiv =document.createElement("div");
-      newDiv.innerHTML="<a>Add Card..</a>";
-      padre.appendChild(newDiv);
-      newDiv.classList.toggle("up");
-      newDiv.addEventListener("click", clickeable);
-    };
-    function añadir(){
-    	var contenedor=document.getElementById("contenedor");
-      var padre3 =document.createElement("section");
-      contenedor.appendChild(padre3);
-      padre3.classList.toggle("newup");
-      var aparecer =document.getElementById("clicky");
-      aparecer.classList.toggle("newup2");
-      padre3.appendChild(aparecer);
-    };
-    function clickeable() {
-    newDiv.classList.add("none");
-  };
-    function clickeable() {
-    newDiv.classList.add("none");
-  };
+      var texto = "<a>Añadir una tarjeta...</a>";
+      var conedorHermano = document.createElement("div");
+      conedorHermano.classList.add("up");
+
+      var padreTemporal = formulario.parentNode;
+
+      contenedorPadre.appendChild(conedorHermano);
+      conedorHermano.appendChild(formulario);
+      conedorHermano.appendChild(div);
+
+      padreTemporal.remove();
+
+      var contenedor = document.createElement("div");
+      contenedor.classList.add("rellenar");
+      contenedor.classList.add("width");
+      contenedor.classList.add("inline-block");
+      contenedor.classList.add("float-left");
+      contenedorPadre.insertBefore(contenedor, contenedorPadre.lastElementChild);
+      
+      agregar(formulario, div);
+
+      crear("div", lista.value, contenedor, "negrita");
+      crear("div", texto, contenedor, "grey");
+    
+      var select = document.querySelectorAll(".grey");
+
+      for(var i=0; i<select.length; i++){
+        select[i].addEventListener("click", function(){
+          this.classList.add("none");
+          crearFormulario("form", contenedor, "formulario", this);
+          mensajes.focus();
+        });
+      }
+
+      lista.value="";
+    });
+
+    var close = document.getElementById("close");
+    close.addEventListener("click", function(e) {
+      e.preventDefault();
+      agregar(formulario, div);
+    });
+
+    function crearFormulario(formulario, padre, clase1, select){
+      var formula = document.createElement(formulario);
+      formula.classList.add(clase1);
+      padre.appendChild(formula);
+
+      crear("textarea", "", formula, "textArea");
+      crear("button", "Guardar", formula, "button");
+
+      formula.lastElementChild.addEventListener("click", function(e){
+        e.preventDefault();
+        select.classList.remove("none");
+
+        var mensajes = document.createElement("div");
+      mensajes.innerHTML=formula.firstElementChild.value;
+        formula.classList.add("none");
+        mensajes.classList.add("mensajes");
+      padre.insertBefore(mensajes, select);
+      })
+    }
+
+    function crear(div, contenido,padre, clase1){
+      var titulo = document.createElement(div);
+      titulo.innerHTML = contenido;
+      titulo.classList.add("padding");
+      titulo.classList.add(clase1);
+      padre.appendChild(titulo);
+    }
+
+    function agregar(formulario, div){
+      formulario.classList.toggle("none");
+      div.classList.toggle("none");
+    }
+
 });
- 
